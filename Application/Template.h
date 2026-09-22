82f394de1c9dfeceaa276743599d9cacf2b26066
P1GUM1 <akinosyoe@gmail.com>
2026-09-22 23:29:55 +0900

    update: Templateの削除

--- Application/Template.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <Windows.h>
-
-int TemplateMain(HINSTANCE hInstance, int nCmdShow);
--- Application/main.cpp
+++ Application/main.cpp
@@ -3,9 +3,9 @@
 #include "../LocalLLM/llamaServer.h"
 #include "../Helper/Helper.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-}
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+//{
+//}
 
 int main()
 {
